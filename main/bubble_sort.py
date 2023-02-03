6aece2f66a9566a7efbb9339d79ca2e192b83d6e
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-02-02 22:11:28 -0500

    Create bubble_sort.py

---
+++ main/bubble_sort.py
@@ -0,0 +1,11 @@
+
+def bubble_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    print(arr)
+
+if __name__ == "__main__":
+    my_arr = [6,1,8,4,9,7,3,2,5]
+    bubble_sort(my_arr)
