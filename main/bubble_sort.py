b55f80ab6b55ca7681b2272cd306e087a4aa919b
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-02-03 23:00:15 -0500

    async

--- main/bubble_sort.py
+++ main/bubble_sort.py
@@ -1,10 +1,14 @@
-from random_generator import random_list
+import time
+from main.random_generator import random_list
 
 def bubble_sort(arr):
+    t = time.process_time()
     for i in range(len(arr) - 1, 0, -1):
         for j in range(i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    elapsed_time = time.process_time() - t
+    print(f"bubble sort {elapsed_time}")
     return arr
 
 if __name__ == "__main__":
--- main/insertion_sort.py
+++ main/insertion_sort.py
@@ -1,10 +1,14 @@
+import time
 
 def insertion_sort(arr: list):
+    t = time.process_time()
     for item in range(1, len(arr)):
         index = item
         while arr[index - 1] > arr[index] and index > 0:
             arr[index - 1], arr[index] = arr[index], arr[index - 1]
             index -= 1
+    elapsed_time = time.process_time() - t
+    print(f"insertion sort {elapsed_time}")
     return arr
 
 if __name__ == "__main__":
