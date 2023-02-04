7ced868fef1ff61b244f2a4a6123d0e111e7b560
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-02-03 20:26:35 -0500

    revers bubble sort, random number generator

--- main/binary_search.py
+++ main/binary_search.py
@@ -1,4 +1,6 @@
 from bubble_sort import bubble_sort
+from random_generator import random_list
+
 
 def binary_search(arr, item):
     first = 0
@@ -13,10 +15,14 @@ def binary_search(arr, item):
                 last = midpoint - 1
             else:
                 first = midpoint + 1
+    print(f"Item found? {found}")
     return found
 
 
 if __name__ == "__main__":
-    my_arr = [5, 2, 6, 9, 3]
+    my_arr = random_list()
+    print(f"INPUT: {my_arr}")
     my_arr = bubble_sort(my_arr)
-    print(binary_search(my_arr, 2))
+    print(f"SORTED: {my_arr}")
+    search_key = input("what do you want to look for? ")
+    binary_search(my_arr, int(search_key))
--- main/reverse_bubble_sort.py
+++ main/reverse_bubble_sort.py
@@ -1,14 +1,14 @@
 from random_generator import random_list
 
-def bubble_sort(arr):
+def reverse_bubble_sort(arr):
     for i in range(len(arr) - 1, 0, -1):
         for j in range(i):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+            if arr[j] < arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 
 if __name__ == "__main__":
     my_arr = random_list()
     print(f"INPUT: {my_arr}")
-    bubble_sort(my_arr)
+    reverse_bubble_sort(my_arr)
     print(f"OUTPUT: {my_arr}")
