1b2096115617cabdc52c446fdc65d195587fda31
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-02-03 21:27:17 -0500

    Update loop_de_loop.py

--- main/loop_de_loop.py
+++ main/loop_de_loop.py
@@ -9,8 +9,13 @@ def double_looping(arr):
         for j in range(i, 0, -1):
             print(j)
 
+def single_enumeration(arr):
+    for i, key in enumerate(arr):
+        print(f"index {i} -- key {key}")
+
 
 if __name__ == "__main__":
     my_arr = random_list()
     single_looping(my_arr)
     double_looping(my_arr)
+    single_enumeration(my_arr)
